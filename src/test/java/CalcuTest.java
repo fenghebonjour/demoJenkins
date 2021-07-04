b4430c68bab22cbed8cf2626badf616359da5ada
fenghe <fenghebonjour@gmail.com>
2021-07-03 21:32:25 -0400

    commit init

---
+++ src/test/java/CalcuTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fengh
+ */
+public class CalcuTest {
+    
+    public CalcuTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Calcu.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = 0;
+        int b = 0;
+        Calcu instance = new Calcu();
+        int expResult = 0;
+        int result = instance.add(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
